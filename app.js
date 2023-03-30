e2c2a0f76c1dba5b63b539c1ae82444500b48746
mertSezerO <117273151+mertSezerO@users.noreply.github.com>
2023-03-30 22:35:02 +0300

    Initial Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const https = require('https');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    
+})
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000")
+})
