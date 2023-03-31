6af9d705de44205e6eb4bdf1a573b264ab0769b7
mertSezerO <117273151+mertSezerO@users.noreply.github.com>
2023-03-31 14:30:25 +0300

    Backend Handling

--- app.js
+++ app.js
@@ -4,7 +4,22 @@ const https = require('https');
 const app = express();
 
 app.get('/', (req, res) => {
-    
+    res.render('index.html')
+})
+
+app.post('/', (req, res) => {
+    const endpoint = 'http://api.openweathermap.org/data/2.5/weather?'
+    const city = 'q=' + req.body.city
+    const key = 'APPID=3e89ae0c8c016e649bb1b3938347fb40'
+    const units = 'units=metric'
+    const url = endpoint + city + '&' + key + '&' + units
+    let weatherData
+    https.get(url, (resp) => {
+        resp.on('data', (data) => {
+            weatherData = JSON.parse(data)
+        })
+    })
+    res.render('response.html', {temp: weatherData.main.temp, desc: weatherData.weather.description, wind: weatherData.wind.speed})
 })
 
 app.listen(3000, () => {
